5bf200c4383ef0476a667d2df9c7fc9894e100c5
Edward Casbon <edward@edwardcasbon.co.uk>
2015-10-12 11:11:39 +0100

    Merged in Confluence theme.

---
+++ src/confluence/js/honeycomb.confluence.js
@@ -0,0 +1,126 @@
+var Honeycomb = Honeycomb || {};
+
+Honeycomb.Confluence = (function($) {
+
+    var sidebar = function sidebar() {
+		$(".confluence-sidebar ul").scrollTree({
+        	'contextPath': window.contextPath,
+          	'css': {
+              'ancestor': 'ancestor',
+              'current': 'active',
+              'collapsed': 'collapsed',
+              'expanded': 'expanded',
+              'toggle': 'toggle'
+            },
+          	'renderChildLi': function (child, opts) {
+              	var html = '<li class="' + opts.css[child.type] + '">';
+              	html += '<a href="' + child.link + '" class="' + opts.css[child.type] + '">';
+
+              	if(typeof child.children !== 'undefined') {
+	              	html += '<span class="' + opts.css.toggle + ' ' + opts.css.toggle + '--has-children"></span>';
+                } else {
+                  	html += '<span class="' + opts.css.toggle + '"></span>';
+                }
+
+              	html += child.title + '</a>';
+              	html += '</li>';
+
+              	return html;
+            }
+        });
+    };
+
+  	var lightbox = function lightbox() {
+      	$(".confluence-embedded-image").each(function(){
+          	var $this = $(this);
+        	var $parent = $this.parent().get(0);
+          	if($parent.nodeName !== "A") {
+            	var $a = $("<a/>")
+                	.addClass("lightbox link-image js-lightbox")
+                	.attr("href", $this.attr("src"))
+                	.attr("rel", "lightbox");
+	            $this.wrap($a);
+            }
+        });
+    };
+
+    var notifications = function notifications() {
+
+        // List of classes to add to.
+        var classes = {
+            "confluence-information-macro": "notification notification--block",
+            "confluence-information-macro-tip": "notification--success",
+            "confluence-information-macro-note": "notification--info",
+            "confluence-information-macro-warning": "notification--fail",
+            "confluence-information-macro-body": "notification__body",
+            "confluence-information-macro-icon": "notification__icon"
+        };
+
+        var icons = {
+            "info": "icon--info",
+            "success": "icon--success",
+            "fail": "icon--fail"
+        };
+
+        // Loop through and add the classes.
+        for(var c in classes) {
+            $("." + c).addClass(classes[c]);
+        }
+
+        // Add the inner container.
+        $(".confluence-information-macro").wrapInner("<div class=\"notification--block__inner-container\"></div>");
+
+        // Loop through adding in notification icons.
+        $(".confluence-information-macro").each(function(){
+            var $this = $(this);
+            for(var i in icons) {
+                if($this.hasClass("notification--" + i)) {
+                    var c = "icon " + icons[i];
+                    $span = $("<span/>").addClass(c);
+                    $span.prependTo($this.find(".confluence-information-macro-icon"));
+                }
+            }
+        });
+    };
+
+    var toc = function toc() {
+        $(".toc-macro").each(function(){
+            var $this = $(this);
+            var defaults = ["h1", "h2", "h3", "h4", "h5", "h6"];
+            var headings = $this.data("headerelements").toLowerCase().split(",");
+            var excludedHeadings = [];
+
+            for(var i=0; i<defaults.length; i++) {
+                if(headings.indexOf(defaults[i]) === -1) {
+                    excludedHeadings.push(defaults[i]);
+                }
+            }
+
+            // Exclude H1 headings by default.
+            excludedHeadings.push("h1");
+
+            // Convert array to string.
+            excludedHeadings = excludedHeadings.join(', ');
+
+            $this.toc({
+                exclude: excludedHeadings,
+                numerate: false
+            });
+        });
+    };
+
+  	var init = function init() {
+    	sidebar();
+      	lightbox();
+        notifications();
+        toc();
+    };
+
+    return {
+        init: init
+    };
+})(jQuery);
+
+/* jQuery(function() {
+  Honeycomb.Confluence.init();
+}); */
---
+++ src/confluence/vendor/jquery.scroll-tree.js
@@ -0,0 +1,136 @@
+(function ($) {
+
+    'use strict';
+
+    // http://learn.jquery.com/plugins/basic-plugin-creation/
+    // http://learn.jquery.com/plugins/advanced-plugin-concepts/
+    $.fn.scrollTree = function (options) {
+
+        var DEFAULT_OPTIONS = {
+            'contextPath': '/',
+            'css': {
+                'ancestor': 'active',
+                'current': 'active',
+                "leaf": 'leaf',
+                'loading': 'sp-loading',
+                'collapsed': 'sp-collapsed',
+                'expanded': 'sp-expanded',
+                'error': 'sp-error',
+              	'toggle': 'sp-toggle'
+            },
+            'renderChildrenUl': function () {
+                return '<ul class="nav"></ul>';
+            },
+            'renderChildLi': function (child, opts) {
+                return '<li class="' + opts.css[child.type] + '"><span class="' + opts.css.toggle + '"></span><a href="' + child.link + '">' + child.title + '</a></li>';
+            }
+        };
+
+        var viewportId = $(this).data('viewportId');
+        var rootLink = $(this).data('root');
+        var currentLink = $(this).data('current');
+
+        var opts = $.extend(true, DEFAULT_OPTIONS, options);
+
+        return this.each(function () {
+            var $rootUl = $(this);
+
+            loadChildren($rootUl, rootLink, currentLink);
+            setupEventHandling($rootUl);
+
+            return this;
+        });
+
+
+        function loadChildren($ul, parentLink, currentLink) {
+            var $parentLi = $ul.closest('li');
+            if ($parentLi) {
+                $parentLi.removeClass(opts.css.collapsed)
+                    .addClass(opts.css.loading);
+            }
+
+            $.get(opts.contextPath + '/rest/scroll-viewport/1.0/tree/children', {
+                'viewportId': viewportId,
+                'root': rootLink,
+                'parent': parentLink || $parentLi.find('> a').attr('href'),
+                'current': currentLink || ''
+            })
+                .success(function success(children) {
+                    insertChildren($ul, children);
+
+                    $parentLi.removeClass(opts.css.loading)
+                        .addClass(opts.css.expanded);
+                })
+                .error(function error(jqXHR, textStatus, errorThrown) {
+                    $parentLi.removeClass(opts.css.loading)
+                        .addClass(opts.css.error);
+                })
+            ;
+        }
+
+
+        function insertChildren($ul, children) {
+            $ul.html('');
+            $.each(children, function (idx, child) {
+                var $childLi = $(opts.renderChildLi(child, opts)).appendTo($ul);
+
+                if (child.children) {
+                    if (child.children.length) {
+                        $childLi.addClass(opts.css.expanded);
+                        var $childrenEl = $(opts.renderChildrenUl()).appendTo($childLi);
+                        insertChildren($childrenEl, child.children);
+
+                    } else {
+                        $childLi.addClass(opts.css.collapsed);
+                    }
+                } else {
+                    $childLi.addClass(opts.css.leaf);
+                }
+            });
+        }
+
+
+        function setupEventHandling($rootUl) {
+	        $rootUl.on('click', '.' + opts.css.toggle, function (e) {
+
+              	var $this = $(this);
+                var $li = $this.parent().parent('li');
+
+              	if($this.hasClass(opts.css.toggle + '--has-children')) {
+                  e.preventDefault();
+                }
+
+                if ($li.is('.' + opts.css.collapsed)) {
+                    openNode($li);
+
+                } else if ($li.is('.' + opts.css.expanded)) {
+                    closeNode($li);
+
+                } else {
+                    // we don't have children -> no-op
+                }
+            });
+        }
+
+
+        function openNode($li) {
+            if ($li.has('ul').length) {
+                // children have been loaded, just toggle classes
+                $li.removeClass(opts.css.collapsed)
+                    .addClass(opts.css.expanded);
+            } else {
+                // children have to be loaded
+                var $childrenEl = $(opts.renderChildrenUl()).appendTo($li);
+                loadChildren($childrenEl);
+            }
+        }
+
+
+        function closeNode($li) {
+            $li
+                .removeClass(opts.css.expanded)
+                .addClass(opts.css.collapsed);
+        }
+    };
+
+})(jQuery);
