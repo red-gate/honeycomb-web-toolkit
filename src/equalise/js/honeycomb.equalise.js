fa5ba2acb556a1a4844243e4df4503bde1cfad3f
philip.scott <philip.scott@red-gate.com>
2016-03-07 15:46:06 +0000

    update jquery.equalise to new version; rerun on browser resize;

--- src/equalise/js/honeycomb.equalise.js
+++ src/equalise/js/honeycomb.equalise.js
@@ -2,14 +2,18 @@ var Honeycomb = Honeycomb || {};
 
 // Equalise heights amongst selected items (https://github.com/edwardcasbon/jquery.equalise)
 Honeycomb.Equalise = function($) {
-  if($.fn.equalise) {
-    $('.js-equal-heights').equalise({
-      itemClass: 'js-equal-heights__item',
-      groupAttr: 'js-equal-heights-group'
-    });
-  }
+	if($.fn.equalise) {
+		$('.js-equal-heights').equalise({
+			itemClass: 'js-equal-heights__item',
+			groupAttr: 'js-equal-heights-group'
+		});
+	}
 };
 
 jQuery(function(){
-  Honeycomb.Equalise(jQuery);
+	$(window).on('resize', function() {
+		Honeycomb.Equalise(jQuery);
+	})
+	// make it run on initial load
+	.trigger('resize');
 });
