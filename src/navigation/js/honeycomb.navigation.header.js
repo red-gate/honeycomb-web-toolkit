d79b960e8fbcad2394eaf3636c19d19fe2fa0a26
Edward Casbon <edward@edwardcasbon.co.uk>
2016-10-18 15:47:05 +0100

    Updated the mobile menu to toggle menu instead of follow link.

--- src/navigation/js/honeycomb.navigation.header.js
+++ src/navigation/js/honeycomb.navigation.header.js
@@ -15,6 +15,19 @@ let init = () => {
             $body.addClass( "mobile-nav--open" );
         }
     });
+
+    // When an item that has a submenu is clicked toggle the menu, rather than
+    // follow the link.
+    $body.on( "click", ".header--primary__menu--mobile .dropdown > a", function( e ) {
+        if ( this.getAttribute( "href" ) !== "#toggle" ) {
+            e.preventDefault();
+
+            let $toggle = $( this ).siblings( `a[ href="#toggle" ]` );
+            if ( $toggle ) {
+                $toggle.trigger( "click" );
+            }
+        }
+    });
 };
 
 export default {
