cdd6752c929081bc31b46d4bb49bb0f8056e197d
Edward Casbon <edward@edwardcasbon.co.uk>
2016-11-14 15:45:20 +0000

    Lazy loaded equalise script.

--- src/equalise/js/honeycomb.equalise.js
+++ src/equalise/js/honeycomb.equalise.js
@@ -1,5 +1,12 @@
+import loadScript from "../../document/js/honeycomb.document.load-script";
+
+let config = {};
+
 // Equalise heights amongst selected items (https://github.com/edwardcasbon/jquery.equalise)
-let init = () => {
+const init = ( cf = {} ) => {
+	
+	config = cf;
+	
 	equalise();
 
 	window.addEventListener( 'resize', () => {
@@ -11,12 +18,23 @@ let init = () => {
 	} );
 };
 
-let equalise = () => {
-	if ( $.fn.equalise ) {
-		$( '.js-equal-heights' ).equalise({
-			itemClass: 'js-equal-heights__item',
-			groupAttr: 'js-equal-heights-group'
-		});
+const equalise = () => {
+	let els = document.querySelectorAll( ".js-equal-heights" );
+	if ( els.length ) {
+		if ( typeof jQuery.fn.equalise !== "function" ) {
+			if ( typeof config.url === "undefined") {
+                config.url = "/src/equalise/vendor/jquery.equalise.min.js";
+            }
+
+            loadScript.load( config.url, () => {
+                init();
+            });
+		} else {
+			jQuery( '.js-equal-heights' ).equalise({
+				itemClass: 'js-equal-heights__item',
+				groupAttr: 'js-equal-heights-group'
+			});
+		}
 	}
 };
 
