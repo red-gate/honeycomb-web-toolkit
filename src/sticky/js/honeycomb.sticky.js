c3fac9b8a17741dc8b05fb74dabe56d27adfb7e7
Edward Casbon <edward@edwardcasbon.co.uk>
2016-11-15 10:01:43 +0000

    Lazy loaded sticky JS

--- src/sticky/js/honeycomb.sticky.js
+++ src/sticky/js/honeycomb.sticky.js
@@ -1,21 +1,35 @@
-// Initialise sticky element functionality. (https://github.com/edwardcasbon/jquery.sticky)
-let init = () => {
-    if ( typeof $.fn.sticky === 'function' ) {
-        $( '.js-sticky' ).each( function () {
-            let $this = $( this );
-            let offset = ( $this.attr( 'data-sticky-offset' ) === 'auto' ) ? 'auto' : parseInt( $this.attr( 'data-sticky-offset' ), 10) || 'auto';
+import loadScript from "../../document/js/honeycomb.document.load-script";
 
-            $this.sticky({
-                offset: offset,
-                sticky: () => {
-                    $this.addClass( 'sticking' );
-                },
-                docked: () => {
-                    $this.removeClass( 'sticking' );
-                },
-                navActiveClass: 'active'
+// Initialise sticky element functionality. (https://github.com/edwardcasbon/jquery.sticky)
+const init = ( config = {} ) => {
+
+    let els = document.querySelectorAll( ".js-sticky" );
+    if ( els.length ) {
+        if ( typeof jQuery.fn.sticky === "undefined" ) {
+            if ( typeof config.url === "undefined") {
+                config.url = "/src/sticky/vendor/jquery.sticky.min.js";
+            }
+
+            loadScript.load( config.url, () => {
+                init();
             });
-        });
+        } else {
+            jQuery( ".js-sticky" ).each( function () {
+                let $this = jQuery( this );
+                let offset = ( $this.attr( "data-sticky-offset" ) === "auto" ) ? "auto" : parseInt( $this.attr( "data-sticky-offset" ), 10) || "auto";
+
+                $this.sticky({
+                    offset: offset,
+                    sticky: () => {
+                        $this.addClass( "sticking" );
+                    },
+                    docked: () => {
+                        $this.removeClass( "sticking" );
+                    },
+                    navActiveClass: "active"
+                });
+            });
+        }    
     }
 };
 
