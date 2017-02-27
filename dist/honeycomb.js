5a97db0920e45faf1d700370d81d97b9132ce137
philscott-rg <philip.scott@red-gate.com>
2017-02-27 15:06:59 +0000

    responsive h1 font-size

--- dist/honeycomb.js
+++ dist/honeycomb.js
@@ -9,7 +9,7 @@ var sites = void 0;
 var settings = void 0;
 
 var init = function init() {
-    var s = arguments.length <= 0 || arguments[0] === undefined ? false : arguments[0];
+    var s = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
 
 
     if (s) {
@@ -82,7 +82,7 @@ var initAccount = function initAccount(accountId) {
 
 // Track a page view.
 var trackPageView = function trackPageView() {
-    var url = arguments.length <= 0 || arguments[0] === undefined ? false : arguments[0];
+    var url = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
 
     if (url) {
         ga("send", "pageview", {
@@ -95,10 +95,10 @@ var trackPageView = function trackPageView() {
 
 // Track an event.
 var trackEvent = function trackEvent() {
-    var category = arguments.length <= 0 || arguments[0] === undefined ? "" : arguments[0];
-    var action = arguments.length <= 1 || arguments[1] === undefined ? "" : arguments[1];
-    var label = arguments.length <= 2 || arguments[2] === undefined ? null : arguments[2];
-    var value = arguments.length <= 3 || arguments[3] === undefined ? null : arguments[3];
+    var category = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "";
+    var action = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "";
+    var label = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+    var value = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : null;
 
     ga("send", "event", category, action, label, value);
 };
@@ -264,7 +264,7 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 var _honeycombDocument = require('../../document/js/honeycomb.document.load-script');
 
@@ -294,7 +294,7 @@ var rearrangeNav = function rearrangeNav(carousel) {
 };
 
 var init = function init() {
-    var config = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+    var config = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
 
     // If no jQuery then break;
@@ -646,7 +646,7 @@ var init = function init() {
 };
 
 var update = function update() {
-    var init = arguments.length <= 0 || arguments[0] === undefined ? false : arguments[0];
+    var init = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
 
     if (updateEls) {
 
@@ -792,8 +792,8 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 var load = function load() {
-    var url = arguments.length <= 0 || arguments[0] === undefined ? false : arguments[0];
-    var callback = arguments.length <= 1 || arguments[1] === undefined ? false : arguments[1];
+    var url = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
+    var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
 
     if (url !== false) {
         (function () {
@@ -873,7 +873,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 // Toggle class when elements in/out of the viewport. (https://github.com/edwardcasbon/jquery.inViewport)
 var init = function init() {
-    var config = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+    var config = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
 
     var vps = document.querySelectorAll(".js-vp");
@@ -918,7 +918,7 @@ var config = {};
 
 // Equalise heights amongst selected items (https://github.com/edwardcasbon/jquery.equalise)
 var init = function init() {
-	var cf = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+	var cf = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
 
 	config = cf;
@@ -1309,7 +1309,7 @@ var _honeycombDocument2 = _interopRequireDefault(_honeycombDocument);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var init = function init() {
-    var config = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+    var config = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
     var els = document.querySelectorAll(".js-lightbox, .js-lightbox--video, .js-lightbox--iframe, .js-lightbox--image, .js-lightbox--inline, .js-lightbox--ajax, .js-lightbox--swf, .js-lightbox--html");
     if (els.length) {
@@ -1964,7 +1964,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 // Initialise sticky element functionality. (https://github.com/edwardcasbon/jquery.sticky)
 var init = function init() {
-    var config = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+    var config = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
 
     var els = document.querySelectorAll(".js-sticky");
@@ -2048,7 +2048,7 @@ var _honeycombDocument2 = _interopRequireDefault(_honeycombDocument);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var init = function init() {
-    var config = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+    var config = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
 
     // If IE7, bail!
@@ -2355,7 +2355,7 @@ var videos = {};
 var analytics = void 0;
 
 var init = function init() {
-    var options = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+    var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
     analytics = options.analytics || false;
     loadYouTubeIframeAPI();
