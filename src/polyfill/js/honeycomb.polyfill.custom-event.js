aef716650a9a60f6de9f33b7ca38bc7cc6a8ea51
philip.scott <philip.scott@red-gate.com>
2017-09-26 11:35:24 +0100

    add a comment on where the customevent polyfill is used

--- src/polyfill/js/honeycomb.polyfill.custom-event.js
+++ src/polyfill/js/honeycomb.polyfill.custom-event.js
@@ -1,5 +1,9 @@
 // polyfill for window.CustomEvent
 // from https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
+
+// this gets used by honeycomb.tabs and honeycomb.reveal
+// honeycomb.reveal fires a CustomEvent which honeycomb.tabs listens for, so that honeycomb.tabs can unset/reset its fixed heights
+
 let CustomEvent = () => {
     if ( typeof window.CustomEvent !== 'function' ) {
         CustomEvent.prototype = window.Event.prototype;
