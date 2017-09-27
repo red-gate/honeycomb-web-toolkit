7337368c79bfca54c9f8ee6cf98910aabde7422b
Edward Casbon <edward@edwardcasbon.co.uk>
2017-09-27 12:03:24 +0100

    Tweaked the padding on deep minimal vertical nav items

--- dist/honeycomb.js
+++ dist/honeycomb.js
@@ -1814,6 +1814,10 @@ Object.defineProperty(exports, "__esModule", {
 });
 // polyfill for window.CustomEvent
 // from https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
+
+// this gets used by honeycomb.tabs and honeycomb.reveal
+// honeycomb.reveal fires a CustomEvent which honeycomb.tabs listens for, so that honeycomb.tabs can unset/reset its fixed heights
+
 var CustomEvent = function CustomEvent() {
     if (typeof window.CustomEvent !== 'function') {
         CustomEvent.prototype = window.Event.prototype;
