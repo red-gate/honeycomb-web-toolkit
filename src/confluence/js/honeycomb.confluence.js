79ec299c3201e9cf59d1af41ab9d6630d0ab6356
Edward Casbon <edward@edwardcasbon.co.uk>
2017-10-23 15:05:33 +0100

    Added Confluence nav JavaScript

--- src/confluence/js/honeycomb.confluence.js
+++ src/confluence/js/honeycomb.confluence.js
@@ -223,6 +223,23 @@ const code = (() => {
     };
 })();
 
+const nav = (() => {
+    const nav = document.querySelector('.nav--vertical--minimal');
+    const container = document.querySelector('.content-container');
+    
+    const init = () => {
+        if (!nav || !container) return false;
+        nav.style.minHeight = '0';     
+        if (window.innerWidth < 600) return false;
+        nav.style.minHeight = `${container.clientHeight}px`;
+    };
+  
+    return {
+        init
+    };
+})();
+window.addEventListener('resize', nav.init);  
+
 const init = () => {
     sidebar();
     lightbox();
@@ -232,6 +249,7 @@ const init = () => {
     convertIds();
     displayVersions();
     code.init();
+    nav.init();
 };
 
 export default {
