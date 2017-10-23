458f5b1676a11346d1e0a5f628c3d00e5649d3ce
Edward Casbon <edward@edwardcasbon.co.uk>
2017-10-23 15:04:50 +0100

    Added Confluence class to ID JavaScript

--- src/confluence/js/honeycomb.confluence.js
+++ src/confluence/js/honeycomb.confluence.js
@@ -145,12 +145,33 @@ const tables = () => {
     }
 };
 
+// Convert classes to IDs (as ID's get stripped out when added
+// via the editor)
+// Usage: set class="class-a class-b id--this-is-my-id class-c"
+const convertIds = () => {
+    const els = document.querySelectorAll('[class*=id--]');
+    for (var i=0; i<els.length; i++) {
+        let id = false;
+        const el = els[i];
+        const cls = el.getAttribute('class').match(/id--[a-z0-9\-]*/ig);
+        if (cls) {
+            const c = cls[0];
+            id = c.replace('id--', '');
+        }
+
+        if (id) {
+            el.setAttribute('id', id);
+        }
+    }
+};
+
 const init = () => {
     sidebar();
     lightbox();
     notifications();
     toc();
     tables();
+    convertIds();
 };
 
 export default {
