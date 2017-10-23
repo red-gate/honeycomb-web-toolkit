e7514ffec974c159a65ed713f1384a654eaefeb5
Edward Casbon <edward@edwardcasbon.co.uk>
2017-10-23 15:05:22 +0100

    Added Confluence code JavaScript

--- src/confluence/js/honeycomb.confluence.js
+++ src/confluence/js/honeycomb.confluence.js
@@ -175,6 +175,54 @@ const displayVersions = () => {
     }
 };
 
+const code = (() => {
+    const titles = document.querySelectorAll('.code__title');
+
+    const init = () => {
+        collapseAll();
+
+        for (var i=0; i<titles.length; i++) {
+            titles[i].addEventListener('click', function() {       
+                const code = this.nextElementSibling;               
+                if (code.style.display === 'none') {
+
+                    // Display code
+                    code.style.display = 'block';
+
+                    this.style.marginBottom = '0';
+                    this.setAttribute('data-code-open', 'true');
+
+                } else {
+
+                    // Hide code
+                    code.style.display = 'none';
+
+                    this.style.marginBottom = '1rem';
+                    this.setAttribute('data-code-open', 'false');
+                }
+            });
+        }
+    };
+
+    const collapseAll = () => {
+        const codes = document.querySelectorAll('.code__title + .prettyprint');
+        
+        for (let i=0; i<codes.length; i++) {
+            codes[i].style.display = 'none';
+        }
+        
+        for (let a=0; a<titles.length; a++) {
+            titles[a].style.marginBottom = '1rem';
+            titles[a].setAttribute('data-code-open', 'false');
+            titles[a].innerHTML += ' <small class="float-right">Toggle source code</small>';
+        }
+    };
+
+    return {
+        init
+    };
+})();
+
 const init = () => {
     sidebar();
     lightbox();
@@ -183,6 +231,7 @@ const init = () => {
     tables();
     convertIds();
     displayVersions();
+    code.init();
 };
 
 export default {
