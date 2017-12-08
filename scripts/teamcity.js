3659c0093bca6ab5309b9001ca5bd264cb3d18b3
Robert Chipperfield <robert.chipperfield@red-gate.com>
2017-12-08 11:33:24 +0000

    Add TC build script (adds service message to set build number)

---
+++ scripts/teamcity.js
@@ -0,0 +1,12 @@
+// Extra tasks when building on TeamCity.
+const pkg = require('../package.json');
+
+// Check we have the BUILD_COUNTER environment variable set - if not, we're probably not running in TC, or
+// it's got lost from the env.* parameters (should be set to %build.counter%)
+if (!process.env.BUILD_COUNTER) {
+	console.error("BUILD_COUNTER environment variable not set: ensure TeamCity project is configured to set this to %build.counter%.");
+	process.exit(1);
+}
+
+// Set the build version from package.json, plus the build counter to uniquify it
+console.log("##teamcity[buildNumber '" + pkg.version + "." + process.env.BUILD_COUNTER + "']");
