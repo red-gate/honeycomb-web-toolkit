4c2fe3b0e11bfb51e5a5fab50fd02df45565ef18
Edward Casbon <edward@edwardcasbon.co.uk>
2018-03-08 11:45:28 +0000

    Added Confluence docs latest version notification

--- src/confluence/js/honeycomb.confluence.js
+++ src/confluence/js/honeycomb.confluence.js
@@ -238,7 +238,72 @@ const nav = (() => {
         init
     };
 })();
-window.addEventListener('resize', nav.init);  
+window.addEventListener('resize', nav.init);
+
+const latestVersionNotification = (() => {
+    const checkForLatestVersion = href => {
+        if (href) {
+            window.jQuery.ajax({
+                url: href,
+                success: data => {
+                    const page = new DOMParser().parseFromString(data, 'text/html');
+                    if (page) {
+                        const docLinks = page.querySelectorAll('[data-macro-name="sp-pagelayout"] a');
+                        let docLink = false;
+                        for (let i=0; i<docLinks.length; i++) {
+                            if (docLinks[i].innerHTML == 'Documentation') {
+                                docLink = docLinks[i];
+                                break;
+                            }
+                        }
+
+                        // Latest docs link is docLink.
+                        const latestSpace = getSpaceFromUrl(docLink.href);
+                        const thisSpace = getSpaceFromUrl(window.location.href);
+                        if (latestSpace !== thisSpace) {
+                            displayNotification(latestSpace, href);
+                        }
+                    }
+                }
+            });
+        }
+    };
+
+    const getSpaceFromUrl = url => {
+        const a = document.createElement('a');
+        a.href = url;
+        const splits = a.pathname.split('/');
+        const space = (splits[0] === '') ? splits[1] : splits[0];
+        return space;
+    };
+
+    const displayNotification = ( latestSpace, docLink ) => {
+        const notification = document.createElement('div');
+        let innerHTML = '';
+
+        notification.className = 'notification notification--block notification--info spaced-top--tight spaced-bottom--none';
+        innerHTML += '<div class="notification--block__inner-container"><figure class="notification__icon"><span class="icon icon--info"></span></figure><div class="notification__body">';
+        innerHTML += `<p>These pages cover ${window.Confluence.Space.name}, which is not the latest version. Help for <a href="${docLink}">other versions</a> is also available.</p>`;
+        innerHTML += '</div></div>';
+        notification.innerHTML = innerHTML;
+
+        const pageContent = document.querySelector('.page-content');
+        if (pageContent) {
+            pageContent.insertBefore(notification, pageContent.firstChild);
+        }
+    };
+
+    const init = () => {
+        const link = document.querySelector('.js-older-versions a');
+        if (link) {
+            checkForLatestVersion(link.href);
+        }
+    };
+
+    return {
+        init
+    };
+})();
 
 const init = () => {
     sidebar();
@@ -250,6 +315,7 @@ const init = () => {
     displayVersions();
     code.init();
     nav.init();
+    latestVersionNotification.init();
 };
 
 export default {
