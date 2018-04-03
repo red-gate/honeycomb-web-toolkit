1406f26faf7f7a00d710e7519553dbbdfba867f3
Edward Casbon <edward@edwardcasbon.co.uk>
2018-04-03 11:39:06 +0100

    Updated confluence tables so that code examples dont get updated to fixed layout.

--- src/confluence/js/honeycomb.confluence.js
+++ src/confluence/js/honeycomb.confluence.js
@@ -131,13 +131,15 @@ const toc = () => {
     });
 };
 
+// If the table isn't a code example (highlight) and is wider
+// than it's parent (i.e. it's wider than the page) then
+// change it to a fixed layout table.
 const tables = () => {
     const tables = document.querySelectorAll('table');
     for (var i=0; i<tables.length; i++) {
         const table = tables[i];
         const wrapper = table.parentElement;
-
-        if (wrapper.className === 'table-wrap') {
+        if (wrapper.className === 'table-wrap' && wrapper.parentElement.className.match('highlight') === null) {
             if (table.clientWidth > wrapper.clientWidth) {
                 table.className += ' table--fixed';
             }
