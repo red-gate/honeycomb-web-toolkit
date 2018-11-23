fa90793511fd328b4c0092445e3e4c7cc53c16e7
Edward Casbon <edward@edwardcasbon.co.uk>
2018-11-23 16:13:57 +0000

    Added an "app" mode with different settings to "website" mode.

--- scripts/css/sass.js
+++ scripts/css/sass.js
@@ -2,40 +2,48 @@ const path = require('path');
 const fs = require('fs-extra');
 const sass = require('node-sass');
 const pkg = require('../../package.json');
-let script = 'honeycomb';
+
+let scripts = [
+    'honeycomb',
+    'honeycomb.app'
+];
 
 // check for overriding arguments
 if( process.argv[2] ) {
-    script = process.argv[2];
+    scripts = [
+        process.argv[2]
+    ];
 }
 
-const css = `${pkg.project.dist}/honeycomb.css`;
-sass.render({
-    file: `${pkg.project.src}/${script}.scss`,
-    outputStyle: 'compressed',
-    outFile: css
-}, ( err, result ) => {
-    if(err) {
-        console.error('Error compiling Sass');
-        console.error(err);
-        process.exit(1);
-    }
-
-    fs.ensureDir(path.dirname(css), err => {
+scripts.forEach(script => {
+    const css = (script === 'zuora' || script === 'confluence') ? `${pkg.project.dist}/honeycomb.css` : `${pkg.project.dist}/${script}.css`;
+    sass.render({
+        file: `${pkg.project.src}/${script}.scss`,
+        outputStyle: 'compressed',
+        outFile: css
+    }, ( err, result ) => {
         if(err) {
-            console.error('Error creating CSS directory');
+            console.error('Error compiling Sass');
             console.error(err);
             process.exit(1);
         }
 
-        fs.writeFile(css, result.css, err => {
+        fs.ensureDir(path.dirname(css), err => {
             if(err) {
-                console.error('Error writing CSS file');
+                console.error('Error creating CSS directory');
                 console.error(err);
                 process.exit(1);
             }
 
-            console.log(`CSS file compiled to ${css}`);
+            fs.writeFile(css, result.css, err => {
+                if(err) {
+                    console.error('Error writing CSS file');
+                    console.error(err);
+                    process.exit(1);
+                }
+
+                console.log(`CSS file compiled to ${css}`);
+            });
         });
     });
 });
