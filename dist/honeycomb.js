a274f832672c35bb69e59a57a0cb20cf65003c76
Phil Scott <philip.scott@red-gate.com>
2019-10-14 15:28:19 +0100

    rebuild and bump version again

--- dist/honeycomb.js
+++ dist/honeycomb.js
@@ -3272,35 +3272,35 @@ var addInlineVideos = function addInlineVideos() {
 var getOptions = function getOptions(video) {
 
     // Copy the defaults.
-    var options = Object.assign({}, options);
+    var optionsCopy = Object.assign({}, options);
 
     // Autohide.
     if (video.hasAttribute('data-video-auto-hide')) {
-        options.autohide = video.getAttribute('data-video-auto-hide');
+        optionsCopy.autohide = video.getAttribute('data-video-auto-hide');
     }
 
     // Autoplay.
     if (video.hasAttribute('data-video-auto-play')) {
-        options.autoplay = video.getAttribute('data-video-auto-play');
+        optionsCopy.autoplay = video.getAttribute('data-video-auto-play');
     }
 
     // Controls.
     if (video.hasAttribute('data-video-controls')) {
-        options.controls = video.getAttribute('data-video-controls');
+        optionsCopy.controls = video.getAttribute('data-video-controls');
     }
 
     // Show info.
     if (video.hasAttribute('data-video-show-info')) {
-        options.showinfo = video.getAttribute('data-video-show-info');
+        optionsCopy.showinfo = video.getAttribute('data-video-show-info');
     }
 
     // Loop.
     if (video.hasAttribute('data-video-loop')) {
-        options.loop = video.getAttribute('data-video-loop');
+        optionsCopy.loop = video.getAttribute('data-video-loop');
     }
 
     // Return the options object.
-    return options;
+    return optionsCopy;
 };
 
 // Add the video when the YouTube iframe API library has loaded.
