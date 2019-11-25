d64f38eb23a931e3e072a4751077519d4d6c1442
Phil Scott <philip.scott@red-gate.com>
2019-11-25 14:26:04 +0000

    calculate the position of the floating context menu list, based on the position of the control
    we want to make sure the context menu appears next to the control, so we calculate its position on click. If the window resizes, we close the lists so we can recalculate on the next click.

--- src/context-menu/js/honeycomb.context-menu.js
+++ src/context-menu/js/honeycomb.context-menu.js
@@ -10,15 +10,48 @@ const init = () => {
 
         document.addEventListener('click', handleClickAway);
     }
+
+    // Close context menus when resizing window (rather than recalculating positioning)
+    window.addEventListener('resize', closeMenus);
+};
+
+// Close all context menus
+const closeMenus = () => {
+    const els = document.querySelectorAll( '.js-context-menu--open' );
+    if ( els.length ) {
+        for ( let i = 0; i < els.length; i++ ) {
+            els[i].classList.remove('js-context-menu--open');
+        }
+    }
 };
 
 // Handler for clicking on the context menu control
 const handleContextMenuControlClick = event => {
     event.preventDefault();
+    const contextMenu = event.target.closest('.js-context-menu');
+
+    // reset position for the list
+    setOffset(contextMenu);
 
     // Toggle context menu open state
-    const contextMenu = event.target.closest('.js-context-menu');
     contextMenu.classList.toggle('js-context-menu--open'); 
+};
+
+// Calculate the horizontal offset for the context menu floating list,
+// based on the position of the control
+const setOffset = contextMenu => {
+    const contextMenuList = contextMenu.querySelector('.js-context-menu__list');
+    const contextMenuControl = contextMenu.querySelector('.js-context-menu__control');
+
+    let left;
+    if ( contextMenu.classList.contains('js-context-menu--right') ) {
+        left = contextMenuControl.offsetLeft - contextMenuControl.offsetWidth;
+    } else {
+        left = contextMenuControl.offsetLeft + 20;
+    }
+
+    contextMenuList.style.left = `${left}px`;
+    
 };
 
 // Handler for clicking away from the context menu
