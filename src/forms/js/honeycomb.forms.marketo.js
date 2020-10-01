fb97801d17b2f5b1af7d7ebe413436b6c41f1bbf
Dave Convery <daveconvery@yahoo.co.uk>
2020-10-01 08:49:39 +0100

    Test Salesforce email validation regex

--- src/forms/js/honeycomb.forms.marketo.js
+++ src/forms/js/honeycomb.forms.marketo.js
@@ -179,9 +179,9 @@ const create = c => {
                         // Email validation.
                         if (typeof fields.Email !== 'undefined') {
 
-                            // Email regex provided by https://regex101.com/r/L9Z2N0/1.
-                            // Check that the format is {something}@{something}.{something}.
-                            const emailRegex = /\S+@\S+\.\S+/;
+                            // Email regex provided by https://developer.salesforce.com/docs/atlas.en-us.noversion.mc-apis.meta/mc-apis/using_regular_expressions_to_validate_email_addresses.htm.
+                            // Check that the format is acceptable to Salesforce (only valid salesforce characters, single @, at least one . character in domain).
+                            const emailRegex = "^[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$" ;
 
                             if (emailRegex.test(fields.Email) === false) {
                                 fail.isFail = true;
