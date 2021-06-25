69238d208e9795db64672a56a90aa06cafcf55d4
Edward Casbon <edward@edwardcasbon.co.uk>
2021-06-25 13:40:18 +0100

    Refactor existing cookie code.

--- src/cookie/js/honeycomb.cookie.js
+++ src/cookie/js/honeycomb.cookie.js
@@ -1,16 +1,23 @@
-let get = ( property ) => {
+/**
+ * Get the value of a cookie.
+ * 
+ * @param {String} property The name of the cookie to get the value for
+ * @returns {String} The value of the cookie
+ */
+const get = property => {
     let value = null;
+    
     if ( document.cookie && document.cookie !== '' ) {
-        let cookies = document.cookie.split( ';' );
+        const cookies = document.cookie.split(';');
         for ( let i = 0; i < cookies.length; i++ ) {
-            // let cookie = cookies[ i ].trim();    // IE9+
-            let cookie = cookies[ i ].replace( /^\s+|\s+$/g, '' );  // IE8
-            if ( cookie.substring( 0, property.length + 1 ) === ( property + '=' ) ) {
-                value = decodeURIComponent( cookie.substring( property.length + 1 ) );
+            let cookie = cookies[i].trim();
+            if ( cookie.substring(0, property.length + 1) === (property + '=') ) {
+                value = decodeURIComponent(cookie.substring(property.length + 1));
                 break;
             }
         }
     }
+    
     return value;
 };
 
@@ -20,6 +27,6 @@ let set = () => {
 };
 
 export default {
-    get: get,
-    set: set
+    get,
+    set,
 };
