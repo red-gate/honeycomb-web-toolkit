17ed825d03aa807c8621f2808eccc45358e15f5a
Edward Casbon <edward@edwardcasbon.co.uk>
2021-06-28 07:28:38 +0100

    Updated cookie functionality (#227)
    * Refactor existing cookie code.
    
    * Added cookie setting functionality.
    
    * Added removing cookie functionality.
    
    * Version bump

--- src/cookie/js/honeycomb.cookie.js
+++ src/cookie/js/honeycomb.cookie.js
@@ -1,25 +1,70 @@
-let get = ( property ) => {
+/**
+ * Get the value of a cookie.
+ * 
+ * @param {String} property The name of the cookie to get the value for
+ * @returns {String} The value of the cookie
+ */
+const get = property => {
     let value = null;
+    
     if ( document.cookie && document.cookie !== '' ) {
-        let cookies = document.cookie.split( ';' );
+        const cookies = document.cookie.split(';');
         for ( let i = 0; i < cookies.length; i++ ) {
-            // let cookie = cookies[ i ].trim();    // IE9+
-            let cookie = cookies[ i ].replace( /^\s+|\s+$/g, '' );  // IE8
-            if ( cookie.substring( 0, property.length + 1 ) === ( property + '=' ) ) {
-                value = decodeURIComponent( cookie.substring( property.length + 1 ) );
+            let cookie = cookies[i].trim();
+            if ( cookie.substring(0, property.length + 1) === (property + '=') ) {
+                value = decodeURIComponent(cookie.substring(property.length + 1));
                 break;
             }
         }
     }
+    
     return value;
 };
 
-// TODO: Write cookie set functionality.
-let set = () => {
-    return '@todo - Need to write cookie set functionality';
+/**
+ * Set a cookie.
+ * 
+ * @param {String} property The name of the cookie to set the value of
+ * @param {String} value The value to set
+ * @param {Object} options An object of options where key is the name and value is the value
+ */
+const set = ( property, value = '', options = {} ) => {
+
+    // Spread default options with user defined.
+    options = {
+        path: '/',
+        ...options
+    };
+
+    // Set the property and value.
+    let updatedCookie = `${property}=${encodeURIComponent(value)}`;
+
+    // Set the options.
+    for ( let optionKey in options ) {
+        updatedCookie += `; ${optionKey}`;
+        let optionValue = options[optionKey];
+        if ( optionValue !== true ) {
+            updatedCookie += `= ${optionValue}`;
+        }
+    }
+
+    // Set the cookie.
+    document.cookie = updatedCookie;
+};
+
+/**
+ * Remove a cookie.
+ * 
+ * @param {String} property The name of the cookie to remove
+ */
+const remove = property => {
+    set(property, '', {
+        'max-age': -1,
+    });
 };
 
 export default {
-    get: get,
-    set: set
+    get,
+    set,
+    remove,
 };
