fb47ffd54056289a6b51debabc9348878b5db0c8
Edward Casbon <edward@edwardcasbon.co.uk>
2021-10-14 14:26:13 +0100

    Compiled JavaScript

--- dist/honeycomb.js
+++ dist/honeycomb.js
@@ -256,7 +256,7 @@ exports["default"] = _default;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.breakpoints = exports.date = exports.version = void 0;
+exports.version = exports.date = exports.breakpoints = void 0;
 var version = 'Version goes here';
 exports.version = version;
 var date = 'Date goes here';
@@ -1552,7 +1552,7 @@ exports["default"] = _default;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = exports.defaults = void 0;
+exports.defaults = exports["default"] = void 0;
 
 var _honeycombDocument = _interopRequireDefault(require("../../document/js/honeycomb.document.load-script"));
 
@@ -2861,6 +2861,8 @@ var scrollOnClick = function scrollOnClick() {
       window.jQuery('html, body').animate({
         scrollTop: window.jQuery(hash).offset().top + offset
       }, 500, function () {
+        window.location.hash = hash;
+
         if (focus) {
           window.jQuery("#".concat(focus)).focus();
         }
