bd82cfc67edebe4b5e5b541894cf689bb2d6b919
Edward Casbon <edward@edwardcasbon.co.uk>
2021-11-23 13:18:47 +0000

    Replace 'request' dependency (#274)
    * Replaced double quotes with single quotes, and converted to LF.
    
    * Added the node-fetch module
    
    * Revert "Added the node-fetch module"
    
    This reverts commit 7c51c3419aa464370ad28c5261e6d1dfd518f872.
    
    * Added node-fetch-commonjs package
    
    * Updated teamcity script with new package APIs.
    
    * Try with request body as plain object
    
    * Add the content type header
    
    * Use URLSearchParams as per docs
    
    * Trying request, to see if that works
    
    * Remove the node-fetch-commonjs depdency

--- scripts/teamcity.js
+++ scripts/teamcity.js
@@ -1,61 +1,62 @@
 // Extra tasks when building on TeamCity.
-const zipFolder = require("zip-a-folder");
-const fs = require("fs");
-const request = require("request");
+const zipFolder = require('zip-a-folder');
+const fs = require('fs');
+const request = require('request');
 const pkg = require('../package.json');
 
 // Check we have the BUILD_COUNTER environment variable set - if not, we're probably not running in TC, or
 // it's got lost from the env.* parameters (should be set to %build.counter%)
 if (!process.env.BUILD_COUNTER) {
-	console.error("BUILD_COUNTER environment variable not set: ensure TeamCity project is configured to set this to %build.counter%.");
-	process.exit(1);
+    console.error('BUILD_COUNTER environment variable not set: ensure TeamCity project is configured to set this to %build.counter%.');
+    process.exit(1);
 }
 if (!process.env.OCTOPUS_URL) {
-	console.error("OCTOPUS_URL environment variable not set.");
-	process.exit(1);
+    console.error('OCTOPUS_URL environment variable not set.');
+    process.exit(1);
 }
 if (!process.env.OCTOPUS_API_KEY) {
-	console.error("OCTOPUS_API_KEY environment variable not set.");
-	process.exit(1);
+    console.error('OCTOPUS_API_KEY environment variable not set.');
+    process.exit(1);
 }
 
 // Set the build version from package.json, plus the build counter to uniquify it
-var packageVersion = pkg.version + "." + process.env.BUILD_COUNTER;
-console.warn("##teamcity[buildNumber '" + packageVersion + "']");
+var packageVersion = pkg.version + '.' + process.env.BUILD_COUNTER;
+console.warn('##teamcity[buildNumber \'' + packageVersion + '\']');
 
 // Zip up the dist folder
-const pkgFilename = "RedGate.HoneycombWebToolkit." + packageVersion + ".zip";
-zipFolder.zipFolder("dist", pkgFilename, function(err) {
-	if(err) {
-		console.error("Failed to zip output package", err);
-		process.exit(1);
-	} else {
-		console.log("Created zipped package");
+const pkgFilename = 'RedGate.HoneycombWebToolkit.' + packageVersion + '.zip';
+(async () => {
+    await zipFolder.zip('dist', pkgFilename);
+    if (fs.existsSync(pkgFilename)) {
+        console.log('Created zipped package');
 
-		// Upload the resulting package to Octopus
-		const packagesEndpoint = process.env.OCTOPUS_URL + "/api/packages/raw";
+        // Upload the resulting package to Octopus
+        const packagesEndpoint = process.env.OCTOPUS_URL + '/api/packages/raw';
 
-		var octopus_post_form = {
-			data: fs.createReadStream(pkgFilename),
-		};
+        const octopus_post_form = {
+            data: fs.createReadStream(pkgFilename),
+        };
 
-		request({
-			url: packagesEndpoint,
-			method: "POST",
-			formData: octopus_post_form,
-			headers: {
-				"X-Octopus-ApiKey": process.env.OCTOPUS_API_KEY
-			}
-		}, function(err, response, body) {
-			// Upload callback
-			if (!err && response.statusCode == 201) {
-				console.log("Package uploaded to Octopus");
-			} else {
-				console.error("Octopus upload failed", err, " status code ", response.statusCode);
-				process.exit(1);
-			}
-		});
-	}
-});
+        request({
+            url: packagesEndpoint,
+            method: 'POST',
+            formData: octopus_post_form,
+            headers: {
+                'X-Octopus-ApiKey': process.env.OCTOPUS_API_KEY
+            }
+        }, function(err, response, body) {
+            // Upload callback
+            if (!err && response.statusCode == 201) {
+                console.log('Package uploaded to Octopus');
+            } else {
+                console.error('Octopus upload failed', err, ' status code ', response.statusCode);
+                process.exit(1);
+            }
+        });
+    } else {
+        console.error('Failed to zip output package');
+        process.exit(1);
+    }
 
-console.log("teamcity.js complete");
+    console.log('teamcity.js complete');
+})();
