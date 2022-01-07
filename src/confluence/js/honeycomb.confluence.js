55c2309d3332c8ca56e13b8419895efe82a7ba9a
Edward Casbon <edward@edwardcasbon.co.uk>
2022-01-07 14:47:53 +0000

    Initialise the cookie consent component for the docs site

--- src/confluence/js/honeycomb.confluence.js
+++ src/confluence/js/honeycomb.confluence.js
@@ -1,16 +1,17 @@
-import Sidebar from './honeycomb.confluence.sidebar';
-import Lightbox from './honeycomb.confluence.lightbox';
-import Notifications from './honeycomb.confluence.notifications';
-import Toc from './honeycomb.confluence.toc';
-import Tables from './honeycomb.confluence.tables';
+import CookieConsent from '../../cookie-consent/js/honeycomb.cookie-consent';
+import Code from './honeycomb.confluence.code';
 import ConvertIds from './honeycomb.confluence.convertIds';
 import DisplayVersions from './honeycomb.confluence.displayVersions';
-import Code from './honeycomb.confluence.code';
-import Nav from './honeycomb.confluence.nav';
-import LatestVersionNotification from './honeycomb.confluence.latestVersionNotification';
 import Expand from './honeycomb.confluence.expand';
-import TaskList from './honeycomb.confluence.tasklist';
+import LatestVersionNotification from './honeycomb.confluence.latestVersionNotification';
+import Lightbox from './honeycomb.confluence.lightbox';
+import Nav from './honeycomb.confluence.nav';
+import Notifications from './honeycomb.confluence.notifications';
 import PageTree from './honeycomb.confluence.pageTree';
+import Sidebar from './honeycomb.confluence.sidebar';
+import Tables from './honeycomb.confluence.tables';
+import TaskList from './honeycomb.confluence.tasklist';
+import Toc from './honeycomb.confluence.toc';
 
 const init = () => {
     Sidebar.init();
@@ -29,6 +30,27 @@ const init = () => {
     Expand.init();
     TaskList.init();
     PageTree.init();
+
+    window.Honeycomb = window.Honeycomb || {};
+    window.Honeycomb.CookieConsent = CookieConsent;
+    CookieConsent.init({
+        banner: {
+            heading: 'Cookies on red-gate.com',
+            links: [{
+                title: 'Privacy notice',
+                href: 'https://www.red-gate.com/website/legal',
+            }],
+        },
+        consentGroups: [
+            // 'functional', // Removing functional for now, as we don't have any yet.
+            'performance',
+            'targeting',
+        ],
+        cookie: {
+            name: 'rgcookieconsent',
+            domain: '.red-gate.com',
+        },
+    });
 };
 
 export default {
