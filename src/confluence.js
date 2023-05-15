46f8941a794f8b6d6e8bdff67092289253f27812
Edward Casbon <edward@edwardcasbon.co.uk>
2023-05-15 14:17:20 +0100

    Init the cookie consent for Confluence sites, namely documentation (#475)

--- src/confluence.js
+++ src/confluence.js
@@ -1,3 +1,29 @@
 // Confluence. (Only import Confluence styling for Confluence themes.)
 import confluence from './confluence/js/honeycomb.confluence';
+import cookieConsent from './cookie-consent/js/honeycomb.cookie-consent';
+
 confluence.init();
+
+// Init the cookie consent functionality, using red-gate.com as the default.
+window.Honeycomb = window.Honeycomb || {};
+window.Honeycomb.CookieConsent = cookieConsent;
+cookieConsent.init({
+    banner: {
+        heading: 'Cookies on red-gate.com',
+        links: [
+            {
+                title: 'Privacy notice',
+                href: 'https://www.red-gate.com/website/legal',
+            },
+        ],
+    },
+    consentGroups: [
+        // 'functional', // Removing functional for now, as we don't have any yet.
+        'performance',
+        'targeting',
+    ],
+    cookie: {
+        name: 'rgcookieconsent',
+        domain: '.red-gate.com',
+    },
+});
